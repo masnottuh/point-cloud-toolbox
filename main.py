c8fcb54ba1805378ecfcce35fc5c71e456ea321d
Gavin <107957601+gavinfunr@users.noreply.github.com>
2024-09-12 17:59:58 -0700

    Update main.py - basic updates
    Fix: generate_pv_shapes() was called incompletely
    Note: I keep getting a segmentation fault at create_mesh_with_curvature() specifically at the estimate normals part.
    [[in utils.py line mesh = o3d.geometry.TriangleMesh()]]
    
    I also am pushing utils.py with more specific logging to show where segmentation fault happens.

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ import os
 ##################################
 logging.basicConfig(level=logging.INFO)
 ##################################
+import sys
+sys.setrecursionlimit(10**9) 
 
-#Validate geometries
-shapes =  generate_pv_shapes()
+# Create test shapes
+shapes =  generate_pv_shapes(num_points=10000, perturbation_strength=0.05)
 
 # List of shape names corresponding to the shapes generated
-shape_names = ["plane", "sphere", "cylinder", "torus", "egg_carton"]
+shape_names = ["plane", "plane_perturbed", "sphere", "sphere_perturbed", "cylinder",
+ "cylinder_perturbed", "torus", "torus_perturbed", "egg_carton", "egg_carton_perturbed"]
 
 # Iterate over the shapes and save points as .ply with descriptive filenames
-for shape, name in zip(shapes, shape_names):
+for shape, shape_names in zip(shapes, shape_names):
     points = shape.points  # Extract points from the shape
-    filename = f"test_shapes/{name}.ply"  # Create a filename based on the shape's name
+    filename = f"test_shapes/{shape_names}.ply"  # Create a filename based on the shape's name
     save_points_to_ply(points, filename)  # Save points to .ply file
     
     # Optionally load and plot the saved shape
     pts = pv.read(filename)
-    pts.plot()
+    pts.plot(text=f'{shape_names}', off_screen=True)
 
 file_directory = "./test_shapes/"
 files = [os.path.join(file_directory, file) for file in os.listdir(file_directory) if file.endswith(".ply")]
